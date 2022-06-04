34a2ead914dfac6a662d185b4683989a6c263d40
Jason Ji <uberjason@gmail.com>
2022-06-04 13:41:32 -0400

    Initial Commit

---
+++ NavigationLinkConfusionAgain/NavigationLinkConfusionAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationLinkConfusionAgainApp.swift
+//  NavigationLinkConfusionAgain
+//
+//  Created by Jason Ji on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationLinkConfusionAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
